aec67788711db9fbbfa51db7987c1871845db373
Sabina <elenasabina@ymail.com>
2021-04-17 17:16:14 +0200

    added Edvinases tests

--- test/BinaryTreeADTNodeTest.java
+++ test/BinaryTreeADTNodeTest.java
@@ -7,12 +7,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class BinaryTreeADTNodeTest {
 
     private BinaryTreeNode treeNode;
-    private int value;
 
     @BeforeEach
-    void setUp(int nodeValue) {
+    void setUp() {
         System.out.println("Test starts");
-        treeNode = new BinaryTreeNode(nodeValue);
+        treeNode = new BinaryTreeNode(10);
     }
 
     @AfterEach
@@ -22,26 +21,40 @@ class BinaryTreeADTNodeTest {
 
     @Test
     void getElement() {
-        assertTrue(treeNode.getElement());
+        assertEquals(treeNode.getElement(),10);
     }
 
     @Test
     void setElement() {
+        treeNode.setElement(15);
+        assertEquals(15,treeNode.getElement());
     }
 
     @Test
     void addLeftChild() {
+        BinaryTreeNode child5 = new BinaryTreeNode(5);
+        treeNode.addLeftChild(child5);
+        assertEquals(5,treeNode.getLeftChild().getElement());
     }
 
     @Test
     void addRightChild() {
+        BinaryTreeNode child3 = new BinaryTreeNode(3);
+        treeNode.addRightChild(child3);
+        assertEquals(3,treeNode.getRightChild().getElement());
     }
 
     @Test
     void getLeftChild() {
+        BinaryTreeNode child5 = new BinaryTreeNode(5);
+        treeNode.addLeftChild(child5);
+        assertEquals(5,treeNode.getLeftChild().getElement());
     }
 
     @Test
     void getRightChild() {
+        BinaryTreeNode child3 = new BinaryTreeNode(3);
+        treeNode.addRightChild(child3);
+        assertEquals(3,treeNode.getRightChild().getElement());
     }
 }
