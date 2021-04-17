c0d526e4ed5fe0c0553ec1a46ed1717c3c785a68
unknown <edvinas.andrijauskas2000@gmail.com>
2021-04-17 15:33:24 +0200

    created folder and started test BinaryTreeNode

---
+++ test/BinaryTreeNodeTest.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinaryTreeNodeTest {
+
+    private BinaryTreeNode treeNode;
+    private int value;
+
+    @BeforeEach
+    void setUp(int nodeValue) {
+        System.out.println("Test starts");
+        treeNode = new BinaryTreeNode(nodeValue);
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("Test ended");
+    }
+
+    @Test
+    void getElement() {
+        assertTrue(treeNode.getElement());
+    }
+
+    @Test
+    void setElement() {
+    }
+
+    @Test
+    void addLeftChild() {
+    }
+
+    @Test
+    void addRightChild() {
+    }
+
+    @Test
+    void getLeftChild() {
+    }
+
+    @Test
+    void getRightChild() {
+    }
+}
